7b7a2bce350fb9a29187f78bf65d6e9dde5e7eac
Bojan Ilievski <drakuwa@gmail.com>
2012-05-29 20:05:36 +0200

    added first activity class

---
+++ src/com/app/sharewifi/ShareWifiActivity.java
@@ -0,0 +1,13 @@
+package com.app.sharewifi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShareWifiActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
