ce630943ec2496b4db859333489e21d2d0c0dd9e
Bojan Ilievski <drakuwa@gmail.com>
2012-06-02 23:48:22 +0200

    removed import of android.util.Log in the Model class

--- src/com/app/sharewifi/Model.java
+++ src/com/app/sharewifi/Model.java
@@ -12,7 +12,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.util.Log;
 
 /**
  * A model class that resolves some of the business logic in the application.
